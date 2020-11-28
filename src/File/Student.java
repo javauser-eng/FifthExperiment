608460cf89b42870fd1e8af79a4e58c5770f6b48
javauser-eng <72697243+javauser-eng@users.noreply.github.com>
2020-11-28 23:28:15 +0800

    Add files via upload

--- src/File/Student.java
+++ src/File/Student.java
@@ -9,15 +9,14 @@ public class Student {
 	int age;						
 	String classes;					
 	String zhuanye;						
-	public Student() {
+	public void m() {
 		this.name = name;
 		this.id = id;
 		this.sex=sex;
 		this.age=  age;
 		this.classes=classes;
 		this.zhuanye=zhuanye;
-	}
-	public static void m(){
+	try{
 		  Scanner scanner=new Scanner(System.in);
 		  System.out.println("输入姓名:");
 		  String name=scanner.nextLine(); 
@@ -27,6 +26,11 @@ public class Student {
 		  String age=scanner.nextLine();
 		  System.out.println("输入学号:");
 		  int id=scanner.nextInt();
-		  System.out.println("姓名:"+name+"\t"+"性别:"+sex+"\t"+"年龄:"+age+"\t"+"学号:"+id);
-		 }
+		  System.out.println("姓名:"+name+"\t"+"性别:"+sex+"\t"+"年龄:"+age+"\t"+"学号:"+id);	
+	}
+	catch(Exception s){
+		System.out.println("检测到您的输入异常，请重新输入:");
+		System.exit(1);
+	}	 
+}
 }
